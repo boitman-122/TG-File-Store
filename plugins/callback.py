be2b6b12015b4835c2ccb377f99371993faa70a4
boitman-122 <114277379+boitman-122@users.noreply.github.com>
2022-09-26 11:44:35 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -13,89 +13,10 @@ from pyrogram.errors import UserNotParticipant
 from config import *
 
 
-@Client.on_callback_query(filters.regex('^help$'))
-async def help_cb(c, m):
-    await m.answer()
-
-    # help text
-    help_text = """**You need Help?? 🧐**
-
-★ Just send me the files i will store file and give you share able link
-
-
-**You can use me in channel too 😉**
-
-★ Make me admin in your channel with edit permission. Thats enough now continue uploading files in channel i will edit all posts and add share able link url buttons
-
-**How to enable uploader details in caption**
-
-★ Use /mode command to change and also you can use `/mode channel_id` to control caption for channel msg."""
-
-    # creating buttons
-    buttons = [
-        [
-            InlineKeyboardButton('Home 🏕', callback_data='home'),
-            InlineKeyboardButton('About 📕', callback_data='about')
-        ],
-        [
-            InlineKeyboardButton('Close 🔐', callback_data='close')
-        ]
-    ]
-
-    # editing as help message
-    await m.message.edit(
-        text=help_text,
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
-
-
 @Client.on_callback_query(filters.regex('^close$'))
 async def close_cb(c, m):
     await m.message.delete()
     await m.message.reply_to_message.delete()
-
-
-@Client.on_callback_query(filters.regex('^about$'))
-async def about_cb(c, m):
-    await m.answer()
-    owner = await c.get_users(int(OWNER_ID))
-    bot = await c.get_me()
-
-    # about text
-    about_text = f"""--**My Details:**--
-
-🤖 𝐌𝐲 𝐍𝐚𝐦𝐞: {bot.mention(style='md')}
-    
-📝 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: [Python 3](https://www.python.org/)
-
-🧰 𝐅𝐫𝐚𝐦𝐞𝐰𝐨𝐫𝐤: [Pyrogram](https://github.com/pyrogram/pyrogram)
-
-👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: {owner.mention(style='md')}
-
-📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/Ns_bot_updates)
-
-👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
-
-🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://github.com/Ns-Bots/TG-File-Store)
-"""
-
-    # creating buttons
-    buttons = [
-        [
-            InlineKeyboardButton('Home 🏕', callback_data='home'),
-            InlineKeyboardButton('Help 💡', callback_data='help')
-        ],
-        [
-            InlineKeyboardButton('Close 🔐', callback_data='close')
-        ]
-    ]
-
-    # editing message
-    await m.message.edit(
-        text=about_text,
-        reply_markup=InlineKeyboardMarkup(buttons),
-        disable_web_page_preview=True
-    )
 
 
 @Client.on_callback_query(filters.regex('^home$'))
