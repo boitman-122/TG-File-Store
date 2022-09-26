2e212cc06c65cb64112b06291dbb3cd5a9952d4b
boitman-122 <114277379+boitman-122@users.noreply.github.com>
2022-09-26 10:10:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True prefe
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 PROTECT_CONTENT = True if os.environ.get("PROTECT_CONTENT", "True") == "True" else False
 UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+DELETE_TIME = int(os.environ.get('DELETE_TIME', 0))
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
