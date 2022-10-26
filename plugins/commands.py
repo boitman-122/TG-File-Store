31e4c92f1544e3060986c41155fd9383faafffbf
boitman-122 <114277379+boitman-122@users.noreply.github.com>
2022-10-26 21:40:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,7 +60,7 @@ async def start(c, m, cb=False):
 
             if string.empty:
                 owner = await c.get_users(int(OWNER_ID))
-                return await m.reply_text(f"🥴 Sorry bro your file was deleted by file owner or bot owner\n\nFor more help contact my owner 👉 {owner.mention(style='md')}")
+                return await m.reply_text(f"🥴 Sorry bro your file was deleted by file owner or bot owner")
             message_ids = (await decode(string.text)).split('-')
             x = []
             for msg_id in message_ids:
@@ -68,7 +68,7 @@ async def start(c, m, cb=False):
 
                 if msg.empty:
                     owner = await c.get_users(int(OWNER_ID))
-                    return await m.reply_text(f"🥴 Sorry bro your file was deleted by file owner or bot owner\n\nFor more help contact my owner 👉 {owner.mention(style='md')}")
+                    return await m.reply_text(f"🥴 Sorry bro your file was deleted by file owner or bot owner")
                 try:
                     y = await msg.copy(m.from_user.id, protect_content=PROTECT_CONTENT)
                     x.append(y)
@@ -94,7 +94,7 @@ async def start(c, m, cb=False):
         msg = await c.get_messages(int(chat_id), int(msg_id)) if not DB_CHANNEL_ID else await c.get_messages(int(DB_CHANNEL_ID), int(msg_id))
 
         if msg.empty:
-            return await send_msg.edit(f"🥴 Sorry bro your file was deleted by file owner or bot owner\n\nFor more help contact my owner 👉 {owner.mention(style='md')}")
+            return await send_msg.edit(f"🥴 Sorry bro your file was deleted by file owner or bot owner.")
         
         caption = f"{msg.caption.markdown}\n\n\n" if msg.caption else ""
         as_uploadername = False
